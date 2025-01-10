8b5ff3c10913df0f3ddc179381f7ad02d5c194cb
Diego Ortega <diego.ortega.dev@gmail.com>
2025-01-10 14:40:03 -0500

    amplify

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["png.pngtree.com","img.freepik.com","static.vecteezy.com"]
-  }
+    domains: ["png.pngtree.com", "img.freepik.com", "static.vecteezy.com"]
+  },
+  distDir: "out"
 };
 
 export default nextConfig;
